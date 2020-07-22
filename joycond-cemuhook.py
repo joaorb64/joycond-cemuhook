1634ccf3d5a51447ddc1860129c82795f9be4f66
João <joaorb64@gmail.com>
2020-07-22 16:53:55 -0300

    Fix Switch Pro Controller dpad not working on Cemu

--- joycond-cemuhook.py
+++ joycond-cemuhook.py
@@ -13,7 +13,7 @@ def clamp(my_value, min_value, max_value):
     return max(min(my_value, max_value), min_value)
 
 def abs_to_button(value):
-    if value > 0.75*255:
+    if value > 0.75:
         value = 255
     else:
         value = 0
